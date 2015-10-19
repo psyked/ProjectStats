e604b86f7a9ad56ff763c4e2b637ddba3fd3f47d
James Ford <james.f@mmtdigital.co.uk>
2015-10-19 09:24:33 +0100

    Neater date formatting

--- parsers/calendarStats.js
+++ parsers/calendarStats.js
@@ -1,16 +1,9 @@
 "use strict";
 
+var moment = require("moment");
+
 function formatDate(date) {
-    var year = date.getFullYear().toString();
-    var month = date.getMonth().toString();
-    if (month.length < 2) {
-        month = "0" + month;
-    }
-    var day = date.getDate().toString();
-    if (day.length < 2) {
-        day = "0" + day;
-    }
-    return year + "-" + month + "-" + day;
+    return moment(date).format("YYYY-MM-DD");
 }
 
 var results = {};
@@ -19,10 +12,7 @@ var results = {};
  * @param {Commit} commit
  */
 function parseCommit(commit) {
-    //console.log(commit.date);
     var date = formatDate(new Date(commit.date));
-    //console.log(formatDate(date));
-    //console.log(date);
     if (!results[date]) {
         results[date] = 0;
     }
