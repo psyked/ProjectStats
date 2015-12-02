1d0c853a785956c001c8bb317abf6b713b12aca5
James Ford <james.f@mmtdigital.co.uk>
2015-12-02 08:09:19 +0000

    Switch to 90 days instead of 365 days

--- website/js-source/main.js
+++ website/js-source/main.js
@@ -8,26 +8,28 @@ require([
     "model/state",
     "lists/commitlist",
     "lists/userlist"
-], function($, crossfilter, d3, History, barChart, getQueryString, state, commitList, userList) {
+], function ($, crossfilter, d3, History, barChart, getQueryString, state, commitList, userList) {
     "use strict";
 
-    (function(window, undefined) {
+    var DAYS = 90;
+
+    (function (window, undefined) {
 
         // Bind to StateChange Event
-        History.Adapter.bind(window, 'statechange', function() { // Note: We are using statechange instead of popstate
+        History.Adapter.bind(window, 'statechange', function () { // Note: We are using statechange instead of popstate
             var State = History.getState(); // Note: We are using History.getState() instead of event.state
         });
 
     })(window);
 
-    d3.csv("output.json", function(error, commits) {
+    d3.csv("output.json", function (error, commits) {
 
         // exclude data from outside the last 365 days
-        commits = commits.filter(function(d) {
-            var startDate = (new Date() - 1000 * 60 * 60 * 24 * 365);
+        commits = commits.filter(function (d) {
+            var startDate = (new Date() - 1000 * 60 * 60 * 24 * DAYS);
             var endDate = (new Date());
             var theDate = parseDate(d.date);
-            if(theDate > startDate && theDate < endDate) {
+            if (theDate > startDate && theDate < endDate) {
                 return true;
             }
             return false;
@@ -37,28 +39,28 @@ require([
         var formatNumber = d3.format(",d");
 
         // A little coercion, since the CSV is untyped.
-        commits.forEach(function(d, i) {
+        commits.forEach(function (d, i) {
             d.index = i;
             d.date = parseDate(d.date);
         });
 
         // Create the crossfilter for the relevant dimensions and groups.
-        var commit = crossfilter(commits), all = commit.groupAll(), date = commit.dimension(function(d) {
+        var commit = crossfilter(commits), all = commit.groupAll(), date = commit.dimension(function (d) {
                 return d.date;
             }), //.filter([new Date() - 1000 * 60 * 60 * 24 * 365, new Date()]),
-            user = commit.dimension(function(d) {
+            user = commit.dimension(function (d) {
                 return d.author;
-            }), dates = date.group(d3.time.day), weeks = date.group(d3.time.week), hour = commit.dimension(function(d) {
+            }), dates = date.group(d3.time.day), weeks = date.group(d3.time.week), hour = commit.dimension(function (d) {
                 return d.date.getHours() + d.date.getMinutes() / 60;
             }), hours = hour.group(Math.floor);
 
-        var dateColWidth = ($(".graph.date").width() - 30) / 365;
+        var dateColWidth = ($(".graph.date").width() - 30) / DAYS;
         var timeColWidth = ($(".graph.time").width() - 30) / 24;
         var charts = [
 
             barChart(dateColWidth).dimension(date).group(dates).round(d3.time.day.round).x(d3.time.scale().domain([
-                new Date() - 1000 * 60 * 60 * 24 * 365, new Date()
-            ]).rangeRound([0, dateColWidth * 365])), //                    .filter([new Date() - 1000 * 60 * 60 * 24 * 365, new Date()]),
+                new Date() - 1000 * 60 * 60 * 24 * DAYS, new Date()
+            ]).rangeRound([0, dateColWidth * DAYS])),
 
             barChart(timeColWidth).dimension(hour).group(hours).x(d3.scale.linear().domain([0, 24]).rangeRound([
                 0, timeColWidth * 24
@@ -69,7 +71,7 @@ require([
         // Given our array of charts, which we assume are in the same order as the
         // .chart elements in the DOM, bind the charts to the DOM and render them.
         // We also listen to the chart's brush events to update the display.
-        var chart = d3.selectAll(".chart .placeholder").data(charts).each(function(chart) {
+        var chart = d3.selectAll(".chart .placeholder").data(charts).each(function (chart) {
             chart.on("brush", renderAll).on("brushend", renderAll);
         });
 
@@ -105,14 +107,14 @@ require([
             return new Date(d);
         }
 
-        window.filter = function(filters) {
-            filters.forEach(function(d, i) {
+        window.filter = function (filters) {
+            filters.forEach(function (d, i) {
                 charts[i].filter(d);
             });
             renderAll();
         };
 
-        window.reset = function(i) {
+        window.reset = function (i) {
             charts[i].filter(null);
 
             //            if (i == 0) {
@@ -128,12 +130,12 @@ require([
             renderAll();
         };
 
-        window.filterUser = function(username) {
+        window.filterUser = function (username) {
             $(".user-info").removeClass("selected");
             state.selectedUsername = username;
-            if(username) {
+            if (username) {
                 $(".user-info[data-user='" + username + "']").addClass("selected");
-                user.filterFunction(function(d) {
+                user.filterFunction(function (d) {
                     return d == username;
                 });
                 $(".panel.user .reset").show();
@@ -147,15 +149,15 @@ require([
 
         var dateFilters, timeFilters;
 
-        if(state.startDate != undefined && state.endDate != undefined) {
+        if (state.startDate != undefined && state.endDate != undefined) {
             dateFilters = [state.startDate, state.endDate];
         }
 
-        if(state.startTime != undefined && state.endTime != undefined) {
+        if (state.startTime != undefined && state.endTime != undefined) {
             timeFilters = [state.startTime, state.endTime];
         }
 
-        if(state.startTime && state.endTime || state.startDate && state.endDate) {
+        if (state.startTime && state.endTime || state.startDate && state.endDate) {
             filter([dateFilters, timeFilters]);
         }
     });
