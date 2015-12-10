9f26f285f5fb3a740c1cb7dc858c5a1ed72dc50b
James Ford <james.f@mmtdigital.co.uk>
2015-12-10 19:35:12 +0000

    Add 4 timespans of leaderboards

--- js-source/dashboard.js
+++ js-source/dashboard.js
@@ -3,25 +3,17 @@ require(["d3", "moment"], function (d3, moment) {
 
     var DAYS = 90;
 
-    function parseDate(d) {
-        return new Date(d);
-    }
-
     d3.csv("output.json", function (error, commits) {
 
         // exclude data from outside the last DAYS days
         commits = commits.filter(function (d) {
-            var startDate = moment().add(-DAYS, 'day').startOf('day');//(new Date() - 1000 * 60 * 60 * 24 * DAYS);
-            var endDate = moment().startOf('day');//(new Date());
-            var theDate = parseDate(d.date);
-
-            if (theDate > startDate && theDate < endDate) {
-                return true;
-            }
-            return false;
+            var startDate = moment().add(-DAYS, 'day').startOf('day');
+            var endDate = moment().startOf('day');
+            var theDate = new Date(d.date);
+            return !!(theDate > startDate && theDate < endDate);
         });
 
-        var nested_data = d3.nest()
+        var yesterday = d3.nest()
             .key(function (d) {
                 return d.author;
             })
@@ -29,22 +21,79 @@ require(["d3", "moment"], function (d3, moment) {
                 return leaves.length;
             })
             .entries(commits.filter(function (d) {
-                var startDate = moment().add(-1, 'day').startOf('day');//(new Date() - 1000 * 60 * 60 * 24 * 1);
-                var endDate = moment().startOf('day');//(new Date());
-                var theDate = parseDate(d.date);
-
-                if (theDate > startDate && theDate < endDate) {
-                    return true;
-                }
-                return false;
+                var startDate = moment().add(-1, 'day').startOf('day');
+                var endDate = moment().startOf('day');
+                var theDate = new Date(d.date);
+                return !!(theDate > startDate && theDate < endDate);
             }))
             .sort(function (a, b) {
                 return d3.descending(a.values, b.values);
             })
             .splice(0, 3);
 
-        nested_data.forEach(function (d, i) {
-            d3.select('.toplist .list').append('div').attr("class", "commit").text(d.key + ' with ' + d.values + ' commits.');
+        yesterday.forEach(function (d, i) {
+            d3.select('.toplist .list.last-day').append('div').attr("class", "commit").html('<span class="name">' +d.key + '</span> with ' + d.values + ' commits.');
+        });
+
+        var lastWeek = d3.nest()
+            .key(function (d) {
+                return d.author;
+            })
+            .rollup(function (leaves) {
+                return leaves.length;
+            })
+            .entries(commits.filter(function (d) {
+                var startDate = moment().add(-1, 'week').startOf('day');
+                var endDate = moment().startOf('day');
+                var theDate = new Date(d.date);
+                return !!(theDate > startDate && theDate < endDate);
+            }))
+            .sort(function (a, b) {
+                return d3.descending(a.values, b.values);
+            })
+            .splice(0, 3);
+
+        lastWeek.forEach(function (d, i) {
+            d3.select('.toplist .list.last-week').append('div').attr("class", "commit").html('<span class="name">' +d.key + '</span> with ' + d.values + ' commits.');
+        });
+
+        var lastMonth = d3.nest()
+            .key(function (d) {
+                return d.author;
+            })
+            .rollup(function (leaves) {
+                return leaves.length;
+            })
+            .entries(commits.filter(function (d) {
+                var startDate = moment().add(-1, 'month').startOf('day');
+                var endDate = moment().startOf('day');
+                var theDate = new Date(d.date);
+                return !!(theDate > startDate && theDate < endDate);
+            }))
+            .sort(function (a, b) {
+                return d3.descending(a.values, b.values);
+            })
+            .splice(0, 3);
+
+        lastMonth.forEach(function (d, i) {
+            d3.select('.toplist .list.last-month').append('div').attr("class", "commit").html('<span class="name">' +d.key + '</span> with ' + d.values + ' commits.');
+        });
+
+        var allTime = d3.nest()
+            .key(function (d) {
+                return d.author;
+            })
+            .rollup(function (leaves) {
+                return leaves.length;
+            })
+            .entries(commits)
+            .sort(function (a, b) {
+                return d3.descending(a.values, b.values);
+            })
+            .splice(0, 3);
+
+        allTime.forEach(function (d, i) {
+            d3.select('.toplist .list.all-time').append('div').attr("class", "commit").html('<span class="name">' +d.key + '</span> with ' + d.values + ' commits.');
         });
 
     });
