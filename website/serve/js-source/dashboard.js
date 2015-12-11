e4f6bfef11ad4b3ef4618195c758bfd197234d25
James Ford <james.f@mmtdigital.co.uk>
2015-12-11 21:50:43 +0000

    Condense the copy & paste code into a single function which renders everything with variables!

--- website/serve/js-source/dashboard.js
+++ website/serve/js-source/dashboard.js
@@ -4,8 +4,96 @@ require(["d3", "moment"], function (d3, moment) {
     var DAYS = 90;
     var COUNT = 10;
 
-    d3.csv("output.json", function (error, commits) {
+    function renderLeaderboard(commits, avatars, panel) {
+        var units = d3.select(panel).attr('data-timeunit');
 
+        if (units) {
+            var previousResults = d3.nest()
+                .key(function (d) {
+                    return d.author;
+                })
+                .rollup(function (leaves) {
+                    return leaves.length;
+                })
+                .entries(commits.filter(function (d) {
+                    var startDate = moment().add(-2, units).startOf(units);
+                    var endDate = moment().add(-1, units).startOf(units);
+                    var theDate = new Date(d.date);
+                    return !!(theDate > startDate && theDate < endDate);
+                }))
+                .sort(function (a, b) {
+                    return d3.descending(a.values, b.values);
+                });
+        }
+
+        var results = d3.nest()
+            .key(function (d) {
+                return d.author;
+            })
+            .rollup(function (leaves) {
+                return leaves.length;
+            })
+            .entries(commits.filter(function (d) {
+                if (units) {
+                    var startDate = moment().add(-1, units).startOf(units);
+                    var endDate = moment().startOf(units);
+                    var theDate = new Date(d.date);
+                    return !!(theDate > startDate && theDate < endDate);
+                } else {
+                    return true;
+                }
+            }))
+            .sort(function (a, b) {
+                return d3.descending(a.values, b.values);
+            })
+            .splice(0, COUNT);
+
+        results.forEach(function (d, i) {
+            var moveIndex = "";
+            if (units) {
+                var oldIndex = previousResults.length;
+                for (var j = 0; j < previousResults.length; j += 1) {
+                    if (previousResults[j]["key"] === d.key) {
+                        oldIndex = j;
+                    }
+                }
+                moveIndex = '<i class="fa fa-minus move-icon"></i>';
+                if (oldIndex > i) {
+                    moveIndex = '<i class="fa fa-arrow-up move-icon up"></i>';
+                } else if (oldIndex < i) {
+                    moveIndex = '<i class="fa fa-arrow-down move-icon down"></i>';
+                }
+            }
+            d3.select(panel).select('.list').append('div').attr("class", "commit").html(moveIndex + ' <span class="index">' + (i + 1) + '.</span> ' + '<span class="name">' + d.key + '</span> with ' + d.values.toLocaleString() + ' commits.');
+        });
+
+        var dataToCheck = results;
+        var firstImage = "";
+        avatars.forEach(function (d, i) {
+            if (d.key === dataToCheck[0].key) {
+                firstImage = d.values[0].key;
+            }
+        });
+        d3.select(panel).select('.avatars-area .first').append('img').attr('src', firstImage);
+
+        var secondImage = "";
+        avatars.forEach(function (d, i) {
+            if (d.key === dataToCheck[1].key) {
+                secondImage = d.values[0].key;
+            }
+        });
+        d3.select(panel).select('.avatars-area .second').append('img').attr('src', secondImage);
+
+        var thirdImage = "";
+        avatars.forEach(function (d, i) {
+            if (d.key === dataToCheck[2].key) {
+                thirdImage = d.values[0].key;
+            }
+        });
+        d3.select(panel).select('.avatars-area .third').append('img').attr('src', thirdImage);
+    }
+
+    d3.csv("output.json", function (error, commits) {
         // exclude data from outside the last DAYS days
         commits = commits.filter(function (d) {
             var startDate = moment().add(-DAYS, 'day').startOf('day');
@@ -27,276 +115,8 @@ require(["d3", "moment"], function (d3, moment) {
             .entries(commits);
 
 
-        var previousDay = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-2, 'day').startOf('day');
-                var endDate = moment().add(-1, 'day').startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            });
-
-        var yesterday = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-1, 'day').startOf('day');
-                var endDate = moment().startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            })
-            .splice(0, COUNT);
-
-        yesterday.forEach(function (d, i) {
-            var oldIndex = previousDay.length;
-            for (var j = 0; j < previousDay.length; j += 1) {
-                if (previousDay[j]["key"] === d.key) {
-                    oldIndex = j;
-                }
-            }
-            var moveIndex = '<i class="fa fa-minus move-icon"></i>';
-            if (oldIndex > i) {
-                moveIndex = '<i class="fa fa-arrow-up move-icon up"></i>';
-            } else if (oldIndex < i) {
-                moveIndex = '<i class="fa fa-arrow-down move-icon down"></i>';
-            }
-            d3.select('.toplist .list.last-day').append('div').attr("class", "commit").html(moveIndex + ' <span class="index">' + (i + 1) + '.</span> ' + '<span class="name">' + d.key + '</span> with ' + d.values + ' commits.');
+        d3.selectAll('.toplist .panel').each(function (d, i) {
+            renderLeaderboard(commits, avatars, this);
         });
-
-        var dataToCheck = yesterday;
-        var firstImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[0].key) {
-                firstImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-day .first').append('img').attr('src', firstImage);
-
-        var secondImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[1].key) {
-                secondImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-day .second').append('img').attr('src', secondImage);
-
-        var thirdImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[2].key) {
-                thirdImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-day .third').append('img').attr('src', thirdImage);
-
-        var previousWeek = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-2, 'week').startOf('day');
-                var endDate = moment().add(-1, 'week').startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            });
-
-        var lastWeek = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-1, 'week').startOf('day');
-                var endDate = moment().startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            })
-            .splice(0, COUNT);
-
-        lastWeek.forEach(function (d, i) {
-            var oldIndex = previousWeek.length;
-            for (var j = 0; j < previousWeek.length; j += 1) {
-                if (previousWeek[j]["key"] === d.key) {
-                    oldIndex = j;
-                }
-            }
-            var moveIndex = '<i class="fa fa-minus move-icon"></i>';
-            if (oldIndex > i) {
-                moveIndex = '<i class="fa fa-arrow-up move-icon up"></i>';
-            } else if (oldIndex < i) {
-                moveIndex = '<i class="fa fa-arrow-down move-icon down"></i>';
-            }
-            d3.select('.toplist .list.last-week').append('div').attr("class", "commit").html(moveIndex + ' <span class="index">' + (i + 1) + '.</span> ' + '<span class="name">' + d.key + '</span> with ' + d.values + ' commits.');
-        });
-
-        var dataToCheck = lastWeek;
-        var firstImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[0].key) {
-                firstImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-week .first').append('img').attr('src', firstImage);
-
-        var secondImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[1].key) {
-                secondImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-week .second').append('img').attr('src', secondImage);
-
-        var thirdImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[2].key) {
-                thirdImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-week .third').append('img').attr('src', thirdImage);
-
-        var previousMonth = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-2, 'month').startOf('day');
-                var endDate = moment().add(-1, 'month').startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            });
-
-        var lastMonth = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits.filter(function (d) {
-                var startDate = moment().add(-1, 'month').startOf('day');
-                var endDate = moment().startOf('day');
-                var theDate = new Date(d.date);
-                return !!(theDate > startDate && theDate < endDate);
-            }))
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            })
-            .splice(0, COUNT);
-
-        lastMonth.forEach(function (d, i) {
-            var oldIndex = previousMonth.length;
-            for (var j = 0; j < previousMonth.length; j += 1) {
-                if (previousMonth[j]["key"] === d.key) {
-                    oldIndex = j;
-                }
-            }
-            var moveIndex = '<i class="fa fa-minus move-icon"></i>';
-            if (oldIndex > i) {
-                moveIndex = '<i class="fa fa-arrow-up move-icon up"></i>';
-            } else if (oldIndex < i) {
-                moveIndex = '<i class="fa fa-arrow-down move-icon down"></i>';
-            }
-            d3.select('.toplist .list.last-month').append('div').attr("class", "commit").html(moveIndex + ' <span class="index">' + (i + 1) + '.</span> ' + '<span class="name">' + d.key + '</span> with ' + d.values + ' commits.');
-        });
-
-        var dataToCheck = lastMonth;
-        var firstImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[0].key) {
-                firstImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-month .first').append('img').attr('src', firstImage);
-
-        var secondImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[1].key) {
-                secondImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-month .second').append('img').attr('src', secondImage);
-
-        var thirdImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[2].key) {
-                thirdImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.last-month .third').append('img').attr('src', thirdImage);
-
-        var allTime = d3.nest()
-            .key(function (d) {
-                return d.author;
-            })
-            .rollup(function (leaves) {
-                return leaves.length;
-            })
-            .entries(commits)
-            .sort(function (a, b) {
-                return d3.descending(a.values, b.values);
-            })
-            .splice(0, COUNT);
-
-        allTime.forEach(function (d, i) {
-            var moveIndex = "";
-            d3.select('.toplist .list.all-time').append('div').attr("class", "commit").html(moveIndex + ' <span class="index">' + (i + 1) + '.</span> ' + '<span class="name">' + d.key + '</span> with ' + d.values + ' commits.');
-        });
-
-        var dataToCheck = allTime;
-        var firstImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[0].key) {
-                firstImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.all-time .first').append('img').attr('src', firstImage);
-
-        var secondImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[1].key) {
-                secondImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.all-time .second').append('img').attr('src', secondImage);
-
-        var thirdImage = "";
-        avatars.forEach(function (d, i) {
-            if (d.key === dataToCheck[2].key) {
-                thirdImage = d.values[0].key;
-            }
-        });
-        d3.select('.avatars-area.all-time .third').append('img').attr('src', thirdImage);
-
     });
 });
