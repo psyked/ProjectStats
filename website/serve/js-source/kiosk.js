eabc9e2ef0fbcccd8ad2c8dbd7a18817e4254be3
James Ford <james.f@mmtdigital.co.uk>
2015-12-13 19:23:56 +0000

    Load a json file and infinite loop some animation for tiles

--- website/serve/js-source/kiosk.js
+++ website/serve/js-source/kiosk.js
@@ -1,18 +1,68 @@
-require(["jquery", "handlebars"], function ($, Handlebars) {
+require(["jquery", "velocity", "handlebars"], function ($, velocity, Handlebars) {
     "use strict";
 
     var source = $("#accolade-template").html();
-    var template = Handlebars.compile(source);
+    var badgeTemplate = Handlebars.compile(source);
+    var badgeData;
 
-    $('.kiosk-container').append(template({
-        badge_img: './img/gremlin.png',
-        title: 'Gremlin',
-        subtitle: 'Don\'t feed them after midnight',
-        description: 'Awarded to developers who really should be tucked up in bed instead of coding after midnight.',
-        recipient: [{
-            avatar: 'https://bitbucket.org/account/jonathanlord/avatar/32/',
-            username: 'Adrian',
-            date: '10th December 2015'
-        }]
-    }));
+    const EASING_PROPS = [0.645, 0.045, 0.355, 1];
+    const EASING_TIME = 500;
+    const HOLD_CARD_TIME = 10000;
+
+    $.ajax({
+        url: './badges.json',
+        success: displayBadges,
+        error: displayError
+    });
+
+    function displayBadges(jsonBadgeData) {
+        badgeData = jsonBadgeData;
+
+        var tile = $(badgeTemplate(badgeData));
+        tile.css('margin-top', '50%');
+
+        $('.kiosk-container').append(tile);
+
+        showCard(tile);
+    }
+
+    function displayError() {
+
+    }
+
+    function showCard(tile) {
+        tile.velocity({
+            marginTop: '0%'
+        }, {
+            duration: EASING_TIME,
+            easing: EASING_PROPS,
+            complete: function animationComplete() {
+                setTimeout(function () {
+                    hideCard(tile);
+                }, HOLD_CARD_TIME);
+            }
+        });
+    }
+
+    function hideCard(tile) {
+        tile.velocity({
+            marginTop: '-50%'
+        }, {
+            duration: EASING_TIME,
+            easing: EASING_PROPS,
+            complete: function(){
+                tile.remove();
+                displayNewCard();
+            }
+        })
+    }
+
+    function displayNewCard() {
+        var tile = $(badgeTemplate(badgeData));
+        tile.css('margin-top', '50%');
+
+        $('.kiosk-container').append(tile);
+
+        showCard(tile);
+    }
 });
