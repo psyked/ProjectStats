3fe27459eefd061beeffc7ab200fd0f8b8cbe344
James Ford <james.f@mmtdigital.co.uk>
2016-05-11 16:27:08 +0100

    Redirect non-https traffic to https on github pages.

--- js-source/kiosk.js
+++ js-source/kiosk.js
@@ -1,5 +1,11 @@
-require(["./components/chromecast-integration", "./components/accolades"], function (chromecast, accolades) {
+require(["./components/chromecast-integration", "./components/accolades"], function(chromecast, accolades) {
     "use strict";
+
+    var host = "psyked.github.io";
+    if((host == window.location.host) && (window.location.protocol != "https:")) {
+        window.location.protocol = "https";
+    }
+
     chromecast();
     accolades();
 });
