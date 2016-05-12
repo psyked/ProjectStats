404bcb3f20da4f9e47dd91f0148e70171b86d7ac
James Ford <james.f@mmtdigital.co.uk>
2016-05-12 21:34:03 +0100

    Switch to a serviceworker with online-first and offline-fallback.

--- js-source/serviceworker.js
+++ js-source/serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-v2';
+const CACHE_NAME = 'cache-online-first';
 
 self.addEventListener("install", function(event) {
     console.log('WORKER: install event in progress.');
@@ -10,13 +10,13 @@ self.addEventListener("activate", function(event) {
     event.waitUntil(
         caches
             .keys()
-            .then(function (keys) {
+            .then(function(keys) {
                 return Promise.all(
                     keys
-                        .filter(function (key) {
+                        .filter(function(key) {
                             return !key.startsWith(CACHE_NAME);
                         })
-                        .map(function (key) {
+                        .map(function(key) {
                             return caches.delete(key);
                         })
                 );
@@ -30,50 +30,21 @@ self.addEventListener("activate", function(event) {
 self.addEventListener("fetch", function(event) {
     console.log('WORKER: fetch event in progress.');
 
-    if (event.request.method !== 'GET') {
+    if(event.request.method !== 'GET') {
         console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
         return;
     }
+
     event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(cached) {
-                var networked = fetch(event.request)
-                    .then(fetchedFromNetwork, unableToResolve)
-                    .catch(unableToResolve);
-
-                console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-                return cached || networked;
-
-                function fetchedFromNetwork(response) {
-                    var cacheCopy = response.clone();
-
-                    console.log('WORKER: fetch response from network.', event.request.url);
-
-                    caches
-                        .open(CACHE_NAME)
-                        .then(function add(cache) {
-                            cache.put(event.request, cacheCopy);
-                        })
-                        .then(function() {
-                            console.log('WORKER: fetch response stored in cache.', event.request.url);
-                        });
-
-                    return response;
-                }
-
-                function unableToResolve () {
-
-                    console.log('WORKER: fetch request failed in both cache and network.');
-
-                    return new Response('<h1>Service Unavailable</h1>', {
-                        status: 503,
-                        statusText: 'Service Unavailable',
-                        headers: new Headers({
-                            'Content-Type': 'text/html'
-                        })
-                    });
-                }
-            })
+        fetch(request).then(function(response) {
+            return caches.open(CACHE_NAME).then(function(cache) {
+                var cacheCopy = response.clone();
+                return cache.put(event.request, cacheCopy);
+            });
+        }).catch(function(error) {
+            return caches.open(CACHE_NAME).then(function(cache) {
+                return cache.match(event.request);
+            });
+        })
     );
 });
