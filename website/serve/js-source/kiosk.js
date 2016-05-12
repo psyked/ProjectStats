7605c79be8c4ee74c5f0eff9420a2bd1fc34517a
James Ford <james.f@mmtdigital.co.uk>
2016-05-12 21:34:22 +0100

    Use serviceworker on all pages.

--- website/serve/js-source/kiosk.js
+++ website/serve/js-source/kiosk.js
@@ -6,6 +6,16 @@ require(["./components/chromecast-integration", "./components/accolades"], funct
         window.location.protocol = "https";
     }
 
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
+
     chromecast();
     accolades();
 });
--- website/serve/js-source/timeline.js
+++ website/serve/js-source/timeline.js
@@ -17,6 +17,16 @@ require([
     if((host == window.location.host) && (window.location.protocol != "https:")) {
         window.location.protocol = "https";
     }
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./serviceworker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    }
     
     const DAYS = 90;
 
