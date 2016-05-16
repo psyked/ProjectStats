bd6f82fc2734314dcbfd1be9f6e9117f5ea7da80
James Ford <james.f@mmtdigital.co.uk>
2016-05-16 08:54:39 +0100

    rebuild

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var CACHE_NAME = 'cache-v2';
+var CACHE_NAME = 'cache-online-first';
 
 self.addEventListener("install", function (event) {
     console.log('WORKER: install event in progress.');
@@ -27,37 +27,15 @@ self.addEventListener("fetch", function (event) {
         console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
         return;
     }
-    event.respondWith(caches.match(event.request).then(function (cached) {
-        var networked = fetch(event.request).then(fetchedFromNetwork, unableToResolve).catch(unableToResolve);
 
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-        return cached || networked;
-
-        function fetchedFromNetwork(response) {
+    event.respondWith(fetch(request).then(function (response) {
+        return caches.open(CACHE_NAME).then(function (cache) {
             var cacheCopy = response.clone();
-
-            console.log('WORKER: fetch response from network.', event.request.url);
-
-            caches.open(CACHE_NAME).then(function add(cache) {
-                cache.put(event.request, cacheCopy);
-            }).then(function () {
-                console.log('WORKER: fetch response stored in cache.', event.request.url);
-            });
-
-            return response;
-        }
-
-        function unableToResolve() {
-
-            console.log('WORKER: fetch request failed in both cache and network.');
-
-            return new Response('<h1>Service Unavailable</h1>', {
-                status: 503,
-                statusText: 'Service Unavailable',
-                headers: new Headers({
-                    'Content-Type': 'text/html'
-                })
-            });
-        }
+            return cache.put(event.request, cacheCopy);
+        });
+    }).catch(function (error) {
+        return caches.open(CACHE_NAME).then(function (cache) {
+            return cache.match(event.request);
+        });
     }));
 });
