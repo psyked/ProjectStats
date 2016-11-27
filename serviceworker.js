258d103c47a832eb242117c61e53a35e42652e4a
James Ford <james.f@mmtdigital.co.uk>
2016-11-27 18:42:46 +0000

    Refactor using jscodemod

--- serviceworker.js
+++ serviceworker.js
@@ -1,41 +1,51 @@
-'use strict';
-
-var CACHE_NAME = 'cache-online-first';
-
-self.addEventListener("install", function (event) {
-    console.log('WORKER: install event in progress.');
-});
-
-self.addEventListener("activate", function (event) {
-    console.log('WORKER: activate event in progress.');
-
-    event.waitUntil(caches.keys().then(function (keys) {
-        return Promise.all(keys.filter(function (key) {
-            return !key.startsWith(CACHE_NAME);
-        }).map(function (key) {
-            return caches.delete(key);
-        }));
-    }).then(function () {
-        console.log('WORKER: activate completed.');
-    }));
-});
-
-self.addEventListener("fetch", function (event) {
-    console.log('WORKER: fetch event in progress.');
-
-    if (event.request.method !== 'GET') {
-        console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
-        return;
-    }
-
-    event.respondWith(fetch(event.request).then(function (response) {
-        return caches.open(CACHE_NAME).then(function (cache) {
-            var cacheCopy = response.clone();
-            return cache.put(event.request, cacheCopy);
-        });
-    }).catch(function (error) {
-        return caches.open(CACHE_NAME).then(function (cache) {
-            return cache.match(event.request);
-        });
-    }));
-});
+// const CACHE_NAME = 'cache-online-first';
+//
+// self.addEventListener("install", function(event) {
+//     console.log('WORKER: install event in progress.');
+// });
+//
+// self.addEventListener("activate", function(event) {
+//     console.log('WORKER: activate event in progress.');
+//
+//     event.waitUntil(
+//         caches
+//             .keys()
+//             .then(function(keys) {
+//                 return Promise.all(
+//                     keys
+//                         .filter(function(key) {
+//                             return !key.startsWith(CACHE_NAME);
+//                         })
+//                         .map(function(key) {
+//                             return caches.delete(key);
+//                         })
+//                 );
+//             })
+//             .then(function() {
+//                 console.log('WORKER: activate completed.');
+//             })
+//     );
+// });
+//
+// self.addEventListener("fetch", function(event) {
+//     console.log('WORKER: fetch event in progress.');
+//
+//     if(event.request.method !== 'GET') {
+//         console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+//         return;
+//     }
+//
+//     event.respondWith(
+//         fetch(event.request).then(function(response) {
+//             return caches.open(CACHE_NAME).then(function(cache) {
+//                 var cacheCopy = response.clone();
+//                 return cache.put(event.request, cacheCopy);
+//             });
+//         }).catch(function(error) {
+//             return caches.open(CACHE_NAME).then(function(cache) {
+//                 return cache.match(event.request);
+//             });
+//         })
+//     );
+// });
+"use strict";
